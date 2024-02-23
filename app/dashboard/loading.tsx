0206d8edf811b6edbe2f35bc0f067b34b4fe7ce8
Facundo Pascale <facundopascale@MacBook-Pro-de-Facundo.local>
2024-02-23 09:07:15 +0100

    doing course

---
+++ app/dashboard/loading.tsx
@@ -0,0 +1,7 @@
+import DashboardSkeleton from '../ui/skeletons';
+
+// esto se usa como loading hasta que se cargue la pantalla, haciendo un archivo loading, next lo utiliza automaticamente
+
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat, Lusitana } from 'next/font/google'
+
+export const montserrat = Montserrat({ subsets: ['latin'], weight: ['400', '500', '600', '700'] })
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
